221a2382e0effbb208c6befe6dc601d22b0c1266
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-08-12 19:33:18 -0300

    atualização

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let nota1 = parseFloat(prompt('Digite a sua nota de ' + materia1));
 if(nota1 <= 0 || nota1 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota1)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia2 = prompt('Digite o nome da matéria');
@@ -17,7 +17,7 @@ let nota2 = parseFloat(prompt('Digite a sua nota de ' + materia2));
 if(nota2 <= 0 || nota2 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota2)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia3 = prompt('Digite o nome da matéria');
@@ -25,7 +25,7 @@ let nota3 = parseFloat(prompt('Digite a sua nota de ' + materia3));
 if(nota3 <= 0 || nota3 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota3)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia4 = prompt('Digite o nome da matéria');
@@ -33,7 +33,7 @@ let nota4 = parseFloat(prompt('Digite a sua nota de ' + materia4));
 if(nota4 <= 0 || nota4 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota4)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia5 = prompt('Digite o nome da matéria');
@@ -41,7 +41,7 @@ let nota5 = parseFloat(prompt('Digite a sua nota de ' + materia5));
 if(nota5 <= 0 || nota5 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota5)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia6 = prompt('Digite o nome da matéria');
@@ -49,7 +49,7 @@ let nota6 = parseFloat(prompt('Digite a sua nota de ' + materia6));
 if(nota6 <= 0 || nota6 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota6)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia7 = prompt('Digite o nome da matéria');
@@ -57,7 +57,7 @@ let nota7 = parseFloat(prompt('Digite a sua nota de ' + materia7)) ;
 if(nota7 <= 0 || nota7 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota7)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia8 = prompt('Digite o nome da matéria');
@@ -65,7 +65,7 @@ let nota8 = parseFloat(prompt('Digite a sua nota de ' + materia8)) ;
 if(nota8 <= 0 || nota8 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota8)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia9 = prompt('Digite o nome da matéria');
@@ -73,7 +73,7 @@ let nota9 = parseFloat(prompt('Digite a sua nota de ' + materia9));
 if(nota9 <= 0 || nota9 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota9)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let materia10 = prompt('Digite o nome da matéria');	
@@ -81,7 +81,7 @@ let nota10 = parseFloat(prompt('Digite a sua nota de ' + materia10)) ;
 if(nota10 <= 0 || nota10 > 10){
     alert('Nota Inválida');
 } else if(isNaN(nota10)){
-    alert('Número inválido')
+    alert('Número inválido');
 }
 
 let somaNotas = nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10;
@@ -92,7 +92,7 @@ let notaPresenca = parseInt(prompt('Digite sua nota de presença')) ;
 if (notaPresenca <= 0 || notaPresenca > 10) {
     alert('Nota Inválida');
 } else if(isNaN(notaPresenca)){
-    alert('Nota inválido')
+    alert('Nota inválido');
 }
 
 if(mediaNotas >= 8 && notaPresenca >= 8){
